b84f7d191d219338331ef18f5dca48af30fe3062
Dan Strong <dangaston@gmail.com>
2017-05-06 06:36:30 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ var breakOut = function(array, changeValue, stopValue) {
 }
 return array
 }
+
+var keepGoing = function(array, changeValue, stopValue) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (array[i] === stopValue) {
+      continue
+  }
+  array[i] = changeValue
+}
+return array
+}
+
+var findBy = function(array, FindFn) {
+  if (returnVal = FindFn(array)) {
+    return returnVal;
+  } else {
+    return null;
+  }
+}
