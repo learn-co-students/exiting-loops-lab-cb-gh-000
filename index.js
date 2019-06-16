d6311fc000d57a95fb661446cf2f36aaa112c8da
Kajal Goel <kajal11122000@gmail.com>
2019-06-16 07:00:26 +0000

    Done.

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ function breakOut(array, changeValue, stopValue){
       break;
     array[i] = changeValue;
   }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue){
+  var i;
+  for(i = 0; i < array.length; i++){
+    if(array[i] === skipValue)
+      continue;
+    array[i] = changeValue;
+  }
+  return array;
+}
+
+function findBy(array, findFn){
+  var i;
+  for(i = 0; i < array.length; i++){
+    if(findFn(array[i]))
+      return array[i];
+  }
+  return null;
 }
