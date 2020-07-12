157f69491db3697ea1fc132a394001292a45c45a
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-07-12 10:48:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i< array.length ; i++) {
+    if (array[i] !== stopValue) {
+      array[i] = changeValue;
+    }
+    else {
+      break;
+    }
+  }
+  return array;
+}
+
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    }
+    else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  let found = 0;
+  var index;
+  for (i; i < array.length; i++){
+    if (findFn(array[i]) === true) {
+      found += 1;
+      index = i;
+      break;
+    }
+  }
+  if (found > 0) {
+    return array[index];
+  }
+  else {
+    return null;
+  }
+}
